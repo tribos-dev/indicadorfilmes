ef42318469bbf90a7fdad3f1851c222958708746
Vinicius Reis <vinicius.reis@msn.com>
2023-05-24 21:18:00 -0300

    init project

---
+++ src/main/java/academy/wakanda/indicadorfilmes/IndicadorfilmesApplication.java
@@ -0,0 +1,13 @@
+package academy.wakanda.indicadorfilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicadorfilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicadorfilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/wakanda/indicadorfilmes/IndicadorfilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.wakanda.indicadorfilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicadorfilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
